ce4caa340ce61904d34a8b848b37dc1561e93d7b
pmo48 <pmoriarty100@gmail.com>
2020-11-07 14:14:14 -0800

    videos html and js

---
+++ Assets/js/vidlogic.js
@@ -0,0 +1,31 @@
+var toneList = ["Queen", "Led Zeppelin", "David Bowie", "Pink Floyd"];
+var tonelistRandom = toneList[Math.floor(Math.random() * toneList.length)];
+console.log(tonelistRandom);
+var APIKey = "AIzaSyDijGkYfBA0SGFZt9yWG70joV9ToxFWMC4";
+// Here we are building the URL we need to query the database
+var queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&maxResults=5&q=" + tonelistRandom + "&type=video&key=" + APIKey;
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+    var toneList = ["Queen", "Led Zeppelin", "David Bowie", "Pink Floyd"];
+    var tonelistRandom = toneList[Math.floor(Math.random() * toneList.length)];
+    console.log(tonelistRandom);
+
+    console.log(response);
+    var randomNumber = Math.floor(Math.random()*50);
+    console.log(randomNumber);
+    var videoName = (response.items[randomNumber].snippet.title)
+    var videoPlay = ("https://www.youtube.com/embed/" + (response.items[randomNumber].id.videoId))
+
+    // // Transfer content to HTML
+    $("#videoName").html("<h1>" + videoName + "</h1>");
+    console.log(videoPlay);
+
+    $("#carouselitem1").attr("src", videoPlay);
+
+  });
