61271e343477ee7d3422ac80d7845916ec55a5db
Evan Alioto <EvanJAlioto@gmail.com>
2020-11-10 19:36:36 -0800

    script.js file now appends sidenav list with local storage Bands values

--- script.js
+++ script.js
@@ -2,33 +2,51 @@ let bandSearch = document.querySelector("#bandSearch");
 let bandSearchButton = document.querySelector("#bandSearchButton");
 let bands = [];
 
-renderLastRegistered();
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
+setPage();
+function setPage() {
+  var storedBands = JSON.parse(localStorage.getItem("bands"));
+  console.log(storedBands);
 
-// function displayMessage() {
-// }
-
-function renderLastRegistered() {
-  var lastBands = localStorage.getItem("bands")
-  
-
-  // Fill in code here to retrieve the last email and password.
-  // If they are null, return early from this function
-  if (!lastBands) {
-    alert("Please Fill out fields")
-    return false;
-  } 
-  // Else set the text of the userEmailSpan and userPasswordSpan 
-    // else (bandSearch.textContent = lastBands);
-  // to the corresponding values form local storage
-  
-}
-bandSearchButton.addEventListener("click", function(event) {
-  event.preventDefault();
-    var bands = document.querySelector("#bandSearch").value;
-  
-  localStorage.setItem("bands", bands);
-  
-  renderLastRegistered();   
+  if(storedBands !== null) {
+    bands = storedBands;
+    renderBandList();
+  } else {
+    localStorage.setItem("bands", "[]");
   }
+}
 
-);
+
+$('#bandSearchButton').on('click',function(event) {
+  event.preventDefault();
+  var bandStorage = $('#bandSearch').val().trim();
+  bands.push(bandStorage);
+  localStorage.setItem("bands", JSON.stringify(bands));
+  $('#bandSearch').empty();
+  renderBandList();
+
+
+
+})
+ function renderBandList() {
+   let cardMarkUp = "";
+
+  for (let i = 0; i < bands.length; i++) {
+    let element = bands[i];
+    cardMarkUp += 
+    `
+    <li class="collection-item avatar">
+    <img src="images/yuna.jpg" alt="" class="circle">
+    <span class="title">${element}</span>
+    <p>First Line <br>
+       Second Line
+    </p>
+    <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+  </li>
+    ` ;
+  } $("#searchedBands").html(cardMarkUp);
+ } 
+
+ 
